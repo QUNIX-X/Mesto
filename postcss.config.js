65bd9be66295e18c9132c79346fa8ae67a1d1ed3
QUNIX <danil07042000@yandex.ru>
2020-02-07 16:05:09 +0500

    fixed style issues with code alignment relative to opening / closing brackets

--- postcss.config.js
+++ postcss.config.js
@@ -3,6 +3,6 @@ module.exports = {
         require('autoprefixer'),
         require('cssnano')({ // подключили cssnano
             preset: 'default', // выбрали настройки по умолчанию
-    })
-]
+        })
+    ]
 }
